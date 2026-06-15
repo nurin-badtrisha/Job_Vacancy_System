6e5674942865feffee1411e709f17fcbde213fb1
elysaeddy <emilyaelysa@gmail.com>
2026-06-15 21:47:50 +0800

    Update jobSearching.php

--- jobSearching.php
+++ jobSearching.php
@@ -1,4 +1,10 @@
 <?php
+session_start(); 
+if (!isset($_SESSION['username'])) {
+    // Terus tendang dia pergi page login balik
+    header("Location: LogIn.php");
+    exit();
+}
 include("dbconn.php");
 
 /* ================= SEARCH & FILTER ================= */
