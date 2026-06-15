e8b0053b2759a9480fd521e7b0273f2c667b2408
elysaeddy <emilyaelysa@gmail.com>
2026-06-15 21:40:59 +0800

    Update LogIn.php

--- LogIn.php
+++ LogIn.php
@@ -1,7 +1,7 @@
 <?php session_start(); 
 if (!isset($_SESSION['username'])) {
     // Terus tendang dia pergi page login balik
-    header("Location: login.php");
+    header("Location: LogIn.php");
     exit();
 }
 ?>
