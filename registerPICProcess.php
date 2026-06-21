fd9f6e26d4a0bb40fc040a3eeea725369f3bb30b
nurin-badtrisha <nbadtrisha@gmail.com>
2026-06-21 12:23:25 +0800

    Update PIC registration to include username

--- registerPICProcess.php
+++ registerPICProcess.php
@@ -9,16 +9,13 @@ if(isset($_POST['Submit'])){
     $company_email   = mysqli_real_escape_string($dbconn, $_POST['company_email']);
     $contact_number   = mysqli_real_escape_string($dbconn, $_POST['company_phone']);
     $username        = mysqli_real_escape_string($dbconn, $_POST['username']);
-    $password        = $_POST['password']; // Will be hashed below
+    $password        =  mysqli_real_escape_string($dbconn, $_POST['password']); 
     $company_address = mysqli_real_escape_string($dbconn, $_POST['company_address']);
     $state           = mysqli_real_escape_string($dbconn, $_POST['state']);
     $city            = mysqli_real_escape_string($dbconn, $_POST['city']);
 
     // Combine city and state for a clean single string database insertion
     $full_address = $company_address . ", " . $city . ", " . $state;
-
-    // Securely hash the password before saving it
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     // 2. INSERT into COMPANY table first
     $sql_company = "INSERT INTO company (company_name, company_email, company_address, contact_number, company_state, company_city) 
@@ -33,13 +30,13 @@ if(isset($_POST['Submit'])){
         $admin_id = isset($_SESSION['admin_id']) ? $_SESSION['admin_id'] : 1; 
 
         // 3. INSERT into PERSON_IN_CHARGE table using the retrieved company ID
-        $sql_pic = "INSERT INTO person_in_charge (admin_id, company_id, email, password) 
-                    VALUES ('$admin_id', '$company_id', '$company_email', '$hashed_password')";
+        $sql_pic = "INSERT INTO person_in_charge (admin_id, company_id, username, password) 
+                    VALUES ('$admin_id', '$company_id', '$username', '$password')";
 
         if(mysqli_query($dbconn, $sql_pic)) {
             echo "<script>
                     alert('PIC & Company Registration Successful!');
-                    window.location.href = 'mainMenu.php';
+                    window.location.href = 'adminReport.php';
                   </script>";
         } else {
             echo "Error creating PIC user account: " . mysqli_error($dbconn);
